92b707753b85273cfaadb184bed3d4fc0fe29c6c
francisco <francisco.arangonz@gmail.com>
2015-02-20 14:11:11 +0100

    seleccion de curso y alumno con errores

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+$('#alumno').focusout(function(){
+$('#infoAlumno').html('Se va a evaluar a ');
+   /*var nomAlumno = $('#alumno option:selected').text();
+    var cursoAlumno = $('#curso option:selected').text();
+    $('#infoAlumno').html('Se va a evaluar a ' + nomAlumno + 'alumno del ciclo de ' + cursoAlumno);*/
+});
