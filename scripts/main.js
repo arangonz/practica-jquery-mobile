466e4618404c0162e6512a5f379aa226e6f2ffc0
francisco <francisco.arangonz@gmail.com>
2015-03-05 20:53:24 +0100

    selec alumno terminado

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-$('#alumno').focusout(function(){
-$('#infoAlumno').html('Se va a evaluar a ');
-   /*var nomAlumno = $('#alumno option:selected').text();
-    var cursoAlumno = $('#curso option:selected').text();
-    $('#infoAlumno').html('Se va a evaluar a ' + nomAlumno + 'alumno del ciclo de ' + cursoAlumno);*/
-});
